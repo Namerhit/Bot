575886606c3a88d62858ae562c251851a30468c4
Namerhit <71889967+Namerhit@users.noreply.github.com>
2021-11-28 23:07:26 +0200

    Add files via upload

--- Translator.py
+++ Translator.py
@@ -38,12 +38,13 @@ async def echo_message(msg: types.Message):
 	temp = list(msg.text)
 	for n in range(0, len(eng)):
 		if(temp[0]==eng[n]):
+		    await msg.reply("Відбувається переклад з древньої мови сітхів на рідну соловїну, зачекайте, будь ласка")
 			result = str()
 			for i in range(0, len(temp)):
 			     	   	for k in range(0, len(eng)):
 			     	   		if(temp[i]==eng[k]):
 			     	   			result = result + ukr[k]
-	await msg.reply("Відбувається переклад з древньої мови сітхів на рідну соловїну, зачекайте, будь ласка")
+	
 	await msg.reply(result)
 	chat_id = msg.chat.id
 	
