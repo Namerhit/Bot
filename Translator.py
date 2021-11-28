35d76923ccf22318be0df9cd144f4d16444d5218
Namerhit <71889967+Namerhit@users.noreply.github.com>
2021-11-28 22:05:16 +0200

    Add files via upload

--- Translator.py
+++ Translator.py
@@ -1,6 +1,8 @@
 from aiogram import Bot, types
 import os
-
+from aiogram.types import ReplyKeyboardRemove, \
+    ReplyKeyboardMarkup, KeyboardButton, \
+    InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor 
 from config import TOKEN 
@@ -8,13 +10,19 @@ bot = Bot(token=TOKEN)
 dp =Dispatcher(bot) 
 
 
+button_hi = KeyboardButton('Привет! ')
 
+greet_kb = ReplyKeyboardMarkup()
+greet_kb.add(button_hi)
 
 eng = ["q", "Q", "w", "W", "e", "E", "r", "R", "t", "T", "y", "Y", "u", "U", "i", "I", "o", "O", "p", "P", "[", "{", "]", "}", "a", "A", "s", "S", "d", "D", "f", "F", "g", "G", "h", "H", "j", "J", "k", "K", "l", "L", ";", ":", "'", '"', "z", "Z", "x", "X", "c", "C", "v", "V", "b", "B", "n", "N", "m", "M", ",", "<", ".", ">", "/", "?", " ", "1", "!", "2", "@", "3", "#", "4", "$", "5", "%", "6", "^", "7", "&", "8", "*", "9", "(", "0", ")", "-", "_", "=", "+"]
 ukr = ["й", "Й", "ц", "Ц", "у", "У", "к", "К", "е", "Е", "н", "Н", "г", "Г", "ш", "Ш", "щ", "Щ", "з", "З", "х", "Х", "ї", "Ї", "ф", "Ф", "і", "І", "в", "В", "а", "А", "п", "П", "р", "Р", "о", "О", "л", "Л", "д", "Д", "ж", "Ж", "є", 'Є', "я", "Я", "ч", "Ч", "с", "С", "м", "М", "и", "И", "т", "Т", "ь", "Ь", "б", "Б", "ю", "Ю", ".", ",", " ", "1", "!", "2", '"', "3", "№", "4", ";", "5", "%", "6", ":", "7", "?", "8", "*", "9", "(", "0", ")", "-", "_", "=", "+"]
 @dp.message_handler(commands=['start'])
 async def process_start_command(message: types.Message):
 	await message.reply("Переводчик для сектантов")
+@dp.message_handler(commands=['greeting'])
+async def process_start_command(message: types.Message):
+	await message.reply("https://youtu.be/dQw4w9WgXcQ")
 @dp.message_handler()
 async def echo_message(msg: types.Message):
 	temp = list(msg.text)
@@ -25,7 +33,8 @@ async def echo_message(msg: types.Message):
 			     	   	for k in range(0, len(eng)):
 			     	   		if(temp[i]==eng[k]):
 			     	   			result = result + ukr[k]
-	await msg.reply(result)
+	await msg.reply("Відбувається переклад з древньої мови сітхів на рідну соловїну, зачекайте, будь ласка")
+	await msg.reply(result, reply_markup=kb.greet_kb)
 	chat_id = msg.chat.id
 	
 
