c32e708ad1b0fd5f70e7ad24cc09cdf5b257a5fb
Namerhit <71889967+Namerhit@users.noreply.github.com>
2022-02-02 18:13:27 +0200

    Add files via upload

--- Translator.py
+++ Translator.py
@@ -12,6 +12,9 @@ dp =Dispatcher(bot)
 keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
 buttons = ["Ага", "Нєа"]
 keyboard.add(*buttons)
+keyboardYesNo = types.ReplyKeyboardMarkup(resize_keyboard=True)
+buttonsYesNo = ["☀️", "☁️"]
+keyboardYesNo.add(*buttonsYesNo)
 
 
 eng = ["q", "Q", "w", "W", "e", "E", "r", "R", "t", "T", "y", "Y", "u", "U", "i", "I", "o", "O", "p", "P", "[", "{", "]", "}", "a", "A", "s", "S", "d", "D", "f", "F", "g", "G", "h", "H", "j", "J", "k", "K", "l", "L", ";", ":", "'", '"', "z", "Z", "x", "X", "c", "C", "v", "V", "b", "B", "n", "N", "m", "M", ",", "<", ".", ">", "/", "?", " ", "!", "@", "#", "$",  "%",  "^", "&",  "*", "=", "+"]
@@ -21,21 +24,10 @@ async def with_puree(message: types.Message):
     chat_id = message.chat.id
     await bot.send_photo(chat_id, photo=open('love.png', 'rb'), reply_markup=types.ReplyKeyboardRemove())
 
-
-@dp.message_handler(Text(equals="Нєа"))
+@dp.message_handler(Text(equals="☀️))
 async def with_puree(message: types.Message):
     chat_id = message.chat.id
-    await bot.send_photo(chat_id, photo=open('fuck.png', 'rb'), reply_markup=types.ReplyKeyboardRemove())
-
-@dp.message_handler(commands=['start'])
-async def process_start_command(message: types.Message):
-	await message.reply("Поки вас не попросять, кнопки не натискати")
-@dp.message_handler(commands=['greeting'])
-async def process_start_command(message: types.Message):
-	await message.reply("https://youtu.be/dQw4w9WgXcQ")
-@dp.message_handler()
-async def echo_message(msg: types.Message):
-	temp = list(msg.text)
+    temp = list(msg.text)
 	for n in range(0, len(eng)):
 		if(temp[0]==eng[n]):
 		    
@@ -53,6 +45,39 @@ async def echo_message(msg: types.Message):
 
 	await bot.send_photo(chat_id, photo=open('Duck.png', 'rb'))
 	await msg.reply("Чи задовольняє вас переклад?", reply_markup=keyboard)
+
+@dp.message_handler(Text(equals="Нєа"))
+async def with_puree(message: types.Message):
+    chat_id = message.chat.id
+    await bot.send_photo(chat_id, photo=open('fuck.png', 'rb'), reply_markup=types.ReplyKeyboardRemove())
+
+@dp.message_handler(commands=['start'])
+async def process_start_command(message: types.Message):
+	await message.reply("Поки вас не попросять, кнопки не натискати")
+@dp.message_handler(commands=['greeting'])
+async def process_start_command(message: types.Message):
+	await message.reply("https://youtu.be/dQw4w9WgXcQ")
+@dp.message_handler()
+async def echo_message(msg: types.Message):
+	await msg.reply("Якщо ви жадаєте перекладу, будь ласка, натисніть на сонечко. Інакше, натисніть на хмаринку.", reply_markup=keyboardYesNo)
+	# temp = list(msg.text)
+	# for n in range(0, len(eng)):
+	# 	if(temp[0]==eng[n]):
+		    
+	# 		result = str()
+	# 		for i in range(0, len(temp)):
+	# 		     	   	for k in range(0, len(eng)):
+	# 		     	   		if(temp[i]==eng[k]):
+	# 		     	   			result = result + ukr[k]
+	# for b in range(0, len(eng)):
+	#     if(temp[0]==eng[b]):
+	#         await msg.reply("Відбувається переклад з древньої мови сітхів на рідну соловїну, зачекайте, будь ласка")
+	# await msg.reply(result)
+	# chat_id = msg.chat.id
+	
+
+	# await bot.send_photo(chat_id, photo=open('Duck.png', 'rb'))
+	# await msg.reply("Чи задовольняє вас переклад?", reply_markup=keyboard)
 
 
 if __name__ == '__main__':
