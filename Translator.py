4d2cefcbc3e560ade09f62d7d0b2ee3f5f86978a
Namerhit <71889967+Namerhit@users.noreply.github.com>
2022-02-02 19:37:45 +0200

    Add files via upload

--- Translator.py
+++ Translator.py
@@ -1,3 +1,4 @@
+from unittest import result
 from aiogram import Bot, types
 import os
 from aiogram.types import ReplyKeyboardRemove, \
@@ -46,28 +47,28 @@ async def process_start_command(message: types.Message):
 @dp.message_handler()
 async def echo_message(msg: types.Message):
 	await msg.reply("Якщо ви жадаєте перекладу, будь ласка, натисніть на сонечко. Інакше, натисніть на хмаринку.", reply_markup=keyboardYesNo)
-
-	@dp.message_handler(Text(equals="☀️"))
-	async def with_puree(message: types.Message):
-		chat_id = message.chat.id
-		temp = list(msg.text)
-		for n in range(0, len(eng)):
-			if(temp[0]==eng[n]):
+	chat_id = msg.chat.id
+	temp = list(msg.text)
+	for n in range(0, len(eng)):
+		if(temp[0]==eng[n]):
 		    
-				result = str()
-				for i in range(0, len(temp)):
-							for k in range(0, len(eng)):
-								if(temp[i]==eng[k]):
-									result = result + ukr[k]
-		for b in range(0, len(eng)):
-			if(temp[0]==eng[b]):
-				await msg.reply("Відбувається переклад з древньої мови сітхів на рідну соловїну, зачекайте, будь ласка")
-		await msg.reply(result)
-		chat_id = msg.chat.id
+			result = str()
+			for i in range(0, len(temp)):
+						for k in range(0, len(eng)):
+							if(temp[i]==eng[k]):
+								result = result + ukr[k]
+	
+
+@dp.message_handler(Text(equals="☀️"))
+async def with_puree(msg: types.Message):
+	await msg.reply("Відбувається переклад з древньої мови сітхів на рідну соловїну, зачекайте, будь ласка")
+	await msg.reply(result)
+	chat_id = msg.chat.id
 	
 	chat_id = msg.chat.id
 	await bot.send_photo(chat_id, photo=open('Duck.png', 'rb'))
 	await msg.reply("Чи задовольняє вас переклад?", reply_markup=keyboard)
+		
 	
 
 
